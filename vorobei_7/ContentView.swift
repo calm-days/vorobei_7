d38b966b005ed68d309e834c96a60ece73882c5c
Roman Liukevich <Raman.Liukevich@ringieraxelspringer.pl>
2024-12-19 17:07:07 +0100

    animation fix

--- vorobei_7/ContentView.swift
+++ vorobei_7/ContentView.swift
@@ -8,50 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var showOpenView = false
+    @State var isPressed: Bool = false
 
     var body: some View {
-        GeometryReader { geometry in
-            let screenWidth = geometry.size.width
-            let screenHeight = geometry.size.height
-
-            VStack {
-                HStack {
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 20)
-                            .fill(.blue)
-                            .frame(width: showOpenView ? 90 : 300,
-                                   height: showOpenView ? 50 : 400)
-
-                        Text(showOpenView
-                             ? "Open"
-                             : "\(Image(systemName: "arrowshape.backward.fill"))  Back")
-                            .foregroundStyle(.white)
-                            .fontWeight(.bold)
-                            .frame(width: 100,
-                                   height: 50)
-                            .offset(x: showOpenView ? 0 : -95,
-                                    y: showOpenView ? 0 : -170)
-                            .animation(.easeInOut(duration: 0.1), value: showOpenView)
-                            .onTapGesture {
-                                showOpenView.toggle()
-                            }
+        VStack {
+            Spacer()
+            HStack {
+                Spacer()
+                ChangableLayout(isPressed: isPressed) {
+                    RoundedRectangle(cornerRadius: isPressed ? 20 : 10)
+                        .foregroundStyle(Color.init(hue: 0.89, saturation: 0.5, brightness: 0.6))
+                    DoubleSizedButton(animation: .easeInOut(duration: 0.55)) {
+                        withAnimation(.easeInOut(duration: 0.55)) {
+                            isPressed.toggle()
+                        }
                     }
-                    .offset(x: showOpenView
-                            ? screenWidth - 135
-                            : screenWidth - 370,
-                            y: showOpenView
-                            ? screenHeight - 90
-                            : screenHeight - 590)
-
-                    .padding()
+                    .layoutPriority(1)
                 }
+                .padding(.bottom, isPressed ? 100 : 10)
+                .padding(.trailing, isPressed ? 40 : 20)
             }
-            .animation(.snappy(duration: 0.45), value: showOpenView)
-
         }
     }
 }
+
+struct ChangableLayout: Layout {
+    let isPressed: Bool
+
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        isPressed
+        ? CGSize(width: proposal.width! * 0.9, height: proposal.height! * 0.6)
+        : subviews.first(where: { $0.priority == 1})!.sizeThatFits(proposal)
+    }
+
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        subviews.forEach {
+            $0.place(at: bounds.origin, proposal: .init(bounds.size))
+        }
+    }
+}
+
+
+struct DoubleSizedButton: View {
+    @State var isPressed: Bool = false
+    let animation: Animation
+    let action: () -> ()
+
+    var body: some View {
+        Button {
+            action()
+            withAnimation(animation) {
+                isPressed.toggle()
+            }
+        } label: {
+            ZStack {
+                Text("\(Image(systemName: "arrowshape.backward.fill")) Back")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .opacity(isPressed ? 1 : 0)
+                    .padding(.vertical, 10)
+                    .padding(.horizontal, 10)
+                Text("Open")
+                    .fontWeight(.bold)
+                    .foregroundStyle(.white)
+                    .opacity(isPressed ? 0 : 1)
+                    .padding(.vertical, 15)
+                    .padding(.horizontal, 30)
+            }
+            .fixedSize(horizontal: false, vertical: true)
+        }
+    }
+}
+
 
 #Preview {
     ContentView()
